20c892d5a979c23aa5ea6e3a6aef67abe45ceb97
Leandro Campolina <leandro.campolina@outlook.com>
2022-11-08 18:17:43 -0300

    refactor: :recycle: change EventShow to dynamic routing
    change EventShow to dynamic routing

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <div id="app">
     <nav>
       <router-link :to="{ name: 'event-list' }">List</router-link> |
-      <router-link :to="{ name: 'event-show' }">Show Event #1</router-link> |
       <router-link :to="{ name: 'event-create' }">Create an Event</router-link>
     </nav>
     <router-view />
