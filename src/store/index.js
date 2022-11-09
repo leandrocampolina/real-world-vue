9c914d8beab07d1b033969ff69dff676e97a5f41
Leandro Campolina <leandro.campolina@outlook.com>
2022-11-09 18:24:23 -0300

    feat: :sparkles: Config state and getters vuex properties
    Config state and getters vuex properties

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,44 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  getters: {},
+  state: {
+    user: { id: "abc123", name: "adam Jahr" },
+    categories: [
+      "sustainability",
+      "nature",
+      "animal welfare",
+      "housing",
+      "education",
+      "food",
+      "comunity",
+    ],
+    // toDos: [
+    //   { id: 1, text: "...", done: true },
+    //   { id: 2, text: "...", done: false },
+    //   { id: 3, text: "...", done: true },
+    //   { id: 4, text: "...", done: false },
+    // ],
+    events: [
+      { id: 1, text: "...", organizer: "..." },
+      { id: 2, text: "...", organizer: "..." },
+      { id: 3, text: "...", organizer: "..." },
+      { id: 4, text: "...", organizer: "..." },
+    ],
+  },
+  getters: {
+    catLength: (state) => {
+      return state.categories.length;
+    },
+    // doneToDos: (state) => {
+    //   return state.toDos.filter((toDo) => toDo.done);
+    // },
+    // activeToDosCount: (state) => {
+    //   return state.toDos.filter((toDo) => !toDo.done).length;
+    // },
+    getEventById: (state) => (id) => {
+      return state.events.find((event) => event.id === id);
+    },
+  },
   mutations: {},
   actions: {},
   modules: {},
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Create event</h1>
+  <div>
+    <h1>Create an event, {{ user.name }}</h1>
+    <p>The envet's creator is: {{ user.id }}</p>
+    <p>Ther are {{ catLength }} of categories</p>
+    <p>{{ getEventById(2) }}</p>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+
+export default {
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength;
+    },
+    ...mapState(["user", "categories"]),
+    ...mapGetters(["getEventById"]),
+  },
+};
+</script>
+
+<style scoped></style>
