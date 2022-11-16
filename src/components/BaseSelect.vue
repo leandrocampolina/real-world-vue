ef7c94e1cb434c137e8fa213cdb224d2052354ab
Leandro Campolina <leandro.campolina@outlook.com>
2022-11-16 16:13:12 -0300

    feat: :sparkles: Add mixins to BaseInput and BaseSelect components
    Add mixins to BaseInput and BaseSelect components

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -20,19 +20,14 @@
 </template>
 
 <script>
+import { formFieldMixin } from "@/mixins/formFieldMixin.js";
+
 export default {
-  inheritAttrs: false,
+  mixins: [formFieldMixin],
   props: {
     options: {
       type: Array,
       required: true,
-    },
-    value: [String, Number],
-    label: String,
-  },
-  methods: {
-    updateValue(event) {
-      this.$emit("input", event.target.value);
     },
   },
 };
